271173e55cf0734b76f3dd5506d89e06f3654510
23hoangkt <hoangkimtruong200oangkimtruong2003@gmail.com>
2025-03-29 00:34:56 +0700

    new

--- scripts/controlKeyboard.py
+++ scripts/controlKeyboard.py
@@ -1,265 +1,113 @@
 #!/usr/bin/env python3
-
-from __future__ import print_function
-
-import threading
-
-import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
-
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import TwistStamped
-
+from std_msgs.msg import Float64
 import sys
-from select import select
+import tty
+import termios
 
-if sys.platform == 'win32':
-    import msvcrt
-else:
-    import termios
-    import tty
+class OmniKeyboardControl:
+    def __init__(self):
+        # Initialize ROS node
+        rospy.init_node('omni_keyboard_control', anonymous=True)
 
+        # Publishers for wheel velocity commands (4 wheels)
+        self.pub_wheel_1 = rospy.Publisher('/wheel_1_joint_controller/command', Float64, queue_size=10)
+        self.pub_wheel_2 = rospy.Publisher('/wheel_2_joint_controller/command', Float64, queue_size=10)
+        self.pub_wheel_3 = rospy.Publisher('/wheel_3_joint_controller/command', Float64, queue_size=10)
+        self.pub_wheel_4 = rospy.Publisher('/wheel_4_joint_controller/command', Float64, queue_size=10)
 
-TwistMsg = Twist
+        # Maximum speed (radians/s)
+        self.max_speed = 10.0  # Adjustable based on requirements
 
-msg = """
-Reading from the keyboard  and Publishing to Twist!
----------------------------
-Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
+        # Current speed of each wheel
+        self.wheel_1_speed = 0.0  # Left (Wheel 1)
+        self.wheel_2_speed = 0.0  # Right (Wheel 2)
+        self.wheel_3_speed = 0.0  # Bottom (Wheel 3)
+        self.wheel_4_speed = 0.0  # Top (Wheel 4)
 
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
+        # Distance from center to wheel (for rotation calculation)
+        self.L = 1.0  # Normalized, adjust if needed
 
-t : up (+z)
-b : down (-z)
-
-anything else : stop
-
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
-
-CTRL-C to quit
-"""
-
-moveBindings = {
-        'i':(1,0,0,0),
-        'o':(1,0,0,-1),
-        'j':(0,0,0,1),
-        'l':(0,0,0,-1),
-        'u':(1,0,0,1),
-        ',':(-1,0,0,0),
-        '.':(-1,0,0,1),
-        'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
-        'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
-        '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
-        't':(0,0,1,0),
-        'b':(0,0,-1,0),
-    }
-
-speedBindings={
-        'q':(1.1,1.1),
-        'z':(.9,.9),
-        'w':(1.1,1),
-        'x':(.9,1),
-        'e':(1,1.1),
-        'c':(1,.9),
-    }
-
-class PublishThread(threading.Thread):
-    def __init__(self, rate):
-        super(PublishThread, self).__init__()
-        self.publisher = rospy.Publisher('/cmd_vel', TwistMsg, queue_size = 1)
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.th = 0.0
-        self.speed = 0.0
-        self.turn = 0.0
-        self.condition = threading.Condition()
-        self.done = False
-
-        # Set timeout to None if rate is 0 (causes new_message to wait forever
-        # for new data to publish)
-        if rate != 0.0:
-            self.timeout = 1.0 / rate
-        else:
-            self.timeout = None
-
-        self.start()
-
-    def wait_for_subscribers(self):
-        i = 0
-        while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
-            if i == 4:
-                print("Waiting for subscriber to connect to {}".format(self.publisher.name))
-            rospy.sleep(0.5)
-            i += 1
-            i = i % 5
-        if rospy.is_shutdown():
-            raise Exception("Got shutdown request before subscribers connected")
-
-    def update(self, x, y, z, th, speed, turn):
-        self.condition.acquire()
-        self.x = x
-        self.y = y
-        self.z = z
-        self.th = th
-        self.speed = speed
-        self.turn = turn
-        # Notify publish thread that we have a new message.
-        self.condition.notify()
-        self.condition.release()
-
-    def stop(self):
-        self.done = True
-        self.update(0, 0, 0, 0, 0, 0)
-        self.join()
+    def get_key(self):
+        # Read a key from the keyboard without requiring Enter
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            key = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return key
 
     def run(self):
-        twist_msg = TwistMsg()
+        rospy.loginfo("Controlling 4-wheel omni robot with keyboard:")
+        rospy.loginfo("w: Forward, s: Backward, a: Left, d: Right, q: Rotate left, e: Rotate right")
+        rospy.loginfo("x: Stop, Ctrl+C: Exit")
 
-        if stamped:
-            twist = twist_msg.twist
-            twist_msg.header.stamp = rospy.Time.now()
-            twist_msg.header.frame_id = twist_frame
-        else:
-            twist = twist_msg
-        while not self.done:
-            if stamped:
-                twist_msg.header.stamp = rospy.Time.now()
-            self.condition.acquire()
-            # Wait for a new message or timeout.
-            self.condition.wait(self.timeout)
+        while not rospy.is_shutdown():
+            key = self.get_key()
 
-            # Copy state into twist message.
-            twist.linear.x = self.x * self.speed
-            twist.linear.y = self.y * self.speed
-            twist.linear.z = self.z * self.speed
-            twist.angular.x = 0
-            twist.angular.y = 0
-            twist.angular.z = self.th * self.turn
+            # Reset speeds to 0 before calculating new values
+            self.wheel_1_speed = 0.0
+            self.wheel_2_speed = 0.0
+            self.wheel_3_speed = 0.0
+            self.wheel_4_speed = 0.0
 
-            self.condition.release()
+            # Define desired velocities (vx, vy, omega)
+            vx = 0.0  # Linear velocity in x (left/right)
+            vy = 0.0  # Linear velocity in y (forward/backward)
+            omega = 0.0  # Angular velocity (rotation)
 
-            # Publish.
-            self.publisher.publish(twist_msg)
+            # Map keyboard inputs to desired velocities
+            if key == 'w':  # Forward
+                vy = self.max_speed
+            elif key == 's':  # Backward
+                vy = -self.max_speed
+            elif key == 'a':  # Left
+                vx = -self.max_speed
+            elif key == 'd':  # Right
+                vx = self.max_speed
+            elif key == 'q':  # Rotate left
+                omega = self.max_speed
+            elif key == 'e':  # Rotate right
+                omega = -self.max_speed
+            elif key == 'x':  # Stop
+                vx = 0.0
+                vy = 0.0
+                omega = 0.0
+            elif key == '\x03':  # Ctrl+C (ASCII code 3)
+                self.wheel_1_speed = 0.0
+                self.wheel_2_speed = 0.0
+                self.wheel_3_speed = 0.0
+                self.wheel_4_speed = 0.0
+                self.pub_wheel_1.publish(self.wheel_1_speed)
+                self.pub_wheel_2.publish(self.wheel_2_speed)
+                self.pub_wheel_3.publish(self.wheel_3_speed)
+                self.pub_wheel_4.publish(self.wheel_4_speed)
+                rospy.loginfo("Stopping robot and exiting")
+                break
 
-        # Publish stop message when thread exits.
-        twist.linear.x = 0
-        twist.linear.y = 0
-        twist.linear.z = 0
-        twist.angular.x = 0
-        twist.angular.y = 0
-        twist.angular.z = 0
-        self.publisher.publish(twist_msg)
+            # Calculate wheel speeds for omni wheels (with corrected directions)
+            self.wheel_1_speed = -vy + omega * self.L
+            self.wheel_2_speed = vy + omega * self.L
+            self.wheel_3_speed = -vx + omega * self.L
+            self.wheel_4_speed = vx + omega * self.L
 
+            # Publish velocity commands
+            self.pub_wheel_1.publish(self.wheel_1_speed)
+            self.pub_wheel_2.publish(self.wheel_2_speed)
+            self.pub_wheel_3.publish(self.wheel_3_speed)
+            self.pub_wheel_4.publish(self.wheel_4_speed)
 
-def getKey(settings, timeout):
-    if sys.platform == 'win32':
-        # getwch() returns a string on Windows
-        key = msvcrt.getwch()
-    else:
-        tty.setraw(sys.stdin.fileno())
-        # sys.stdin.read() returns a string on Linux
-        rlist, _, _ = select([sys.stdin], [], [], timeout)
-        if rlist:
-            key = sys.stdin.read(1)
-        else:
-            key = ''
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
+            # Display current status
+            rospy.loginfo(f"Wheel 1 (Left): {self.wheel_1_speed:.2f}, Wheel 2 (Right): {self.wheel_2_speed:.2f}, "
+                          f"Wheel 3 (Bottom): {self.wheel_3_speed:.2f}, Wheel 4 (Top): {self.wheel_4_speed:.2f}")
 
-def saveTerminalSettings():
-    if sys.platform == 'win32':
-        return None
-    return termios.tcgetattr(sys.stdin)
+            rospy.sleep(0.1)  # Prevent reading keys too quickly
 
-def restoreTerminalSettings(old_settings):
-    if sys.platform == 'win32':
-        return
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-
-def vels(speed, turn):
-    return "currently:\tspeed %s\tturn %s " % (speed,turn)
-
-if __name__=="__main__":
-    settings = saveTerminalSettings()
-
-    rospy.init_node('teleop_twist_keyboard')
-
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
-    speed_limit = rospy.get_param("~speed_limit", 1000)
-    turn_limit = rospy.get_param("~turn_limit", 1000)
-    repeat = rospy.get_param("~repeat_rate", 0.0)
-    key_timeout = rospy.get_param("~key_timeout", 0.5)
-    stamped = rospy.get_param("~stamped", False)
-    twist_frame = rospy.get_param("~frame_id", '')
-    if stamped:
-        TwistMsg = TwistStamped
-
-    pub_thread = PublishThread(repeat)
-
-    x = 0
-    y = 0
-    z = 0
-    th = 0
-    status = 0
-
+if __name__ == '__main__':
     try:
-        pub_thread.wait_for_subscribers()
-        pub_thread.update(x, y, z, th, speed, turn)
-
-        print(msg)
-        print(vels(speed,turn))
-        while(1):
-            key = getKey(settings, key_timeout)
-            if key in moveBindings.keys():
-                x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                z = moveBindings[key][2]
-                th = moveBindings[key][3]
-            elif key in speedBindings.keys():
-                speed = min(speed_limit, speed * speedBindings[key][0])
-                turn = min(turn_limit, turn * speedBindings[key][1])
-                if speed == speed_limit:
-                    print("Linear speed limit reached!")
-                if turn == turn_limit:
-                    print("Angular speed limit reached!")
-                print(vels(speed,turn))
-                if (status == 14):
-                    print(msg)
-                status = (status + 1) % 15
-            else:
-                # Skip updating cmd_vel if key timeout and robot already
-                # stopped.
-                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
-                    continue
-                x = 0
-                y = 0
-                z = 0
-                th = 0
-                if (key == '\x03'):
-                    break
-
-            pub_thread.update(x, y, z, th, speed, turn)
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        pub_thread.stop()
-        restoreTerminalSettings(settings)
+        controller = OmniKeyboardControl()
+        controller.run()
+    except rospy.ROSInterruptException:
+        pass
